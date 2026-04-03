d80ed0b1fa2ea49d55f880f48189e248bd3be66d
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-04-03 11:27:27 +0530

    feat: add mouse offest so that middle of element is counted rather than mouse pointer

--- scripts/eventHandlers/dragOverEvent.js
+++ scripts/eventHandlers/dragOverEvent.js
@@ -1,3 +1,6 @@
+import { getMiddleHeight } from "../services/getMiddle.js";
+import { getMouseOffset } from "../services/mouseOffest.js";
+
 export default function dragOverEvent() {
     const containers = document.querySelectorAll(".task-list-container");
 
@@ -6,18 +9,16 @@ export default function dragOverEvent() {
             e.preventDefault();
 
             const draggingEle = document.querySelector(".dragging");
-            const mouseY = e.clientY;
 
             const children = [...container.lastElementChild.children];
-
+            let mouseOffest = getMouseOffset();
+            const mouseY = e.pageY + mouseOffest.y;
             const nearestIndex = children.reduce((acc, item, index) => {
                 if (item.classList.contains("dragging")) return acc;
                 if (acc === -1) return index;
-                const accRect = children[acc].getBoundingClientRect();
-                const curRect = item.getBoundingClientRect();
 
-                const accMid = accRect.top + accRect.height / 2;
-                const curMid = curRect.top + curRect.height / 2;
+                const accMid = getMiddleHeight(children[acc]);
+                const curMid = getMiddleHeight(item);
 
                 if (Math.abs(mouseY - curMid) < Math.abs(mouseY - accMid)) {
                     return index;
@@ -27,8 +28,10 @@ export default function dragOverEvent() {
             }, -1);
 
             if (nearestIndex !== -1) {
-                let rect = children[nearestIndex].getBoundingClientRect();
-                if (rect.top + rect.height / 2 > e.clientY) {
+                let nearestMiddleHeight = getMiddleHeight(
+                    children[nearestIndex],
+                );
+                if (nearestMiddleHeight > mouseY) {
                     children[nearestIndex].before(draggingEle);
                 } else {
                     children[nearestIndex].after(draggingEle);
---
+++ scripts/services/mouseOffest.js
@@ -0,0 +1,12 @@
+let mouseOffset = {
+    x: 0,
+    y: 0,
+};
+
+export function getMouseOffset() {
+    return mouseOffset;
+}
+
+export function setMouseOffset(offset) {
+    mouseOffset = offset;
+}
