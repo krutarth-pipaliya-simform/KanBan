ec693332c50af7c563f8f276fa7dd23bb4b57155
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-04-06 15:04:40 +0530

    feat: add touch events to make it work on phone too

--- scripts/eventHandlers/dragOverEvent.js
+++ scripts/eventHandlers/dragOverEvent.js
@@ -1,5 +1,8 @@
-import { addContainerHighlight, removeContainerHighlight } from "../services/containerHighlight.js";
-import { getMiddleHeight} from "../services/getMiddle.js";
+import {
+    addContainerHighlight,
+    removeContainerHighlight,
+} from "../services/containerHighlight.js";
+import { getMiddleHeight } from "../services/getMiddle.js";
 import getNearestIndex from "../services/getNearestIndex.js";
 import { getMouseOffset } from "../services/mouseOffest.js";
 import syncListWithLocalstorage from "../services/syncListWithLocalstorage.js";
@@ -13,6 +16,36 @@ export default function dragOverEvent() {
         const mouseOffest = getMouseOffset();
         const mouseX = e.clientX + mouseOffest.x;
         const mouseY = e.clientY + mouseOffest.y;
+
+        const containers = [...wrapper.children];
+        const container = containers[getNearestIndex(mouseX, containers, true)];
+        const children = [...container.lastElementChild.children];
+
+        const draggingEle = document.querySelector(".dragging");
+        const nearestIndex = getNearestIndex(mouseY, children, false);
+
+        if (nearestIndex !== -1) {
+            let nearestMiddleHeight = getMiddleHeight(children[nearestIndex]);
+            if (nearestMiddleHeight > mouseY) {
+                children[nearestIndex].before(draggingEle);
+            } else {
+                children[nearestIndex].after(draggingEle);
+            }
+            removeContainerHighlight();
+            addContainerHighlight(container);
+        } else {
+            container.lastElementChild.appendChild(draggingEle);
+        }
+
+        syncListWithLocalstorage();
+    });
+
+    wrapper.addEventListener("touchmove", (e) => {
+        e.preventDefault();
+
+        const mouseOffest = getMouseOffset();
+        const mouseX = e.touches[0].clientX + mouseOffest.x;
+        const mouseY = e.touches[0].clientY + mouseOffest.y;
 
         const containers = [...wrapper.children];
         const container = containers[getNearestIndex(mouseX, containers, true)];
--- scripts/services/addDragEvents.js
+++ scripts/services/addDragEvents.js
@@ -11,9 +11,20 @@ export default function addDragEvent(elem) {
             y: getMiddleHeight(elem) - e.pageY,
         });
     });
+    elem.addEventListener('touchstart', (e) => {
+        addDraggingClass(elem);
+        setMouseOffset({
+            x: getMiddleWidth(elem) - e.touches[0].pageX,
+            y: getMiddleHeight(elem) - e.touches[0].pageY,
+        });
+    })
     //add for drag end
     elem.addEventListener("dragend", () => {
         removeDraggingClass(elem);
         setMouseOffset({ x: 0, y: 0 });
     });
+    elem.addEventListener('touchend', () => {
+        removeDraggingClass(elem);
+        setMouseOffset({ x: 0, y: 0 });
+    })
 }
